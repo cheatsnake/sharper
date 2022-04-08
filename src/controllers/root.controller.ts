86fb84ac9794a7e15ce22a6694d280340cebe76c
Yury <72691412+cheatsnake@users.noreply.github.com>
2022-04-08 10:58:43 +0300

    💬 Update root link

--- src/controllers/root.controller.ts
+++ src/controllers/root.controller.ts
@@ -3,7 +3,7 @@ import { Request, Response } from "express";
 export class RootController {
     async redirectToDocs(req: Request, res: Response) {
         try {
-            res.redirect("https://github.com/cheatsnake");
+            res.redirect("https://github.com/cheatsnake/sharper");
         } catch (error) {
             console.log(error);
         }
